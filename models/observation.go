319dee2271b5e33f00f9f8b98c591736fb38e451
Maxence Henneron <maxence.henneron@icloud.com>
2018-06-29 13:12:08 +0000

    fix(circular-import): fix circular import

--- models/observation.go
+++ models/observation.go
@@ -2,15 +2,14 @@ package models
 
 import (
 	"time"
-	"gitlab.com/plugblocks/iothings-api/models/schema_org"
 )
 
 type Observation struct {
-	Id         string     `json:"id" bson:"_id"`
-	Timestamp  int64      `json:"timestamp" bson:"timestamp" valid:"-"`
-	DeviceId   string     `json:"device_id" bson:"device_id"`
-	Type       string     `json:"type" bson:"type"`
-	Values []schema_org.QuantitativeValue `json:"values" bson:"values"`
+	Id        string     `json:"id" bson:"_id"`
+	Timestamp int64      `json:"timestamp" bson:"timestamp" valid:"-"`
+	DeviceId  string     `json:"device_id" bson:"device_id"`
+	Type      string     `json:"type" bson:"type"`
+	Values    []Property `json:"values" bson:"values"`
 }
 
 func (o *Observation) BeforeCreate(device *Device) {
