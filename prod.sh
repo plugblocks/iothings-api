c01f97bec3ee249bc2712e1b16bf142b1e7bcde2
Adrien Chapelet <adrien3d@gmail.com>
2018-08-29 15:56:51 +0200

    Modify prod.sh according to demo plb

--- prod.sh
+++ prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 openssl genrsa -out key.rsa 2048
 openssl base64 -in key.rsa -out key64.rsa
-docker run --name mongo-db -d mongo
-docker build -t iothings-api .
-docker run --name iothings -p 127.0.0.1:4000:4000 --link mongo-db:mongo -d iothings-api
+docker run --name demo-plb-mongo -d mongo
+docker build -t api-demo-plb-img .
+docker run --name demo-plb-api -p 127.0.0.1:4000:4000 --link demo-plb-mongo:mongo -d api-demo-plb-img
