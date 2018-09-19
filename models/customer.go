b288661fa03018d58a439371d95057625b011c94
Maxence Henneron <maxence.henneron@icloud.com>
2018-09-19 16:48:12 +0200

    fix various problems

--- models/customer.go
+++ models/customer.go
@@ -5,6 +5,7 @@ type Customer struct {
 	FirstName string `json:"first_name" bson:"first_name"`
 	LastName string `json:"last_name" bson:"last_name"`
 	Address GeoCoordinates `json:"address" bson:"address"`
+	Email string `json:"email" bson:"email"`
 	PhoneNumber string `json:"phone_number" bson:"phone_number"`
 	OrganizationId string `json:"organization_id" bson:"organization_id"`
 }
