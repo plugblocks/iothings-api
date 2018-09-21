53ea96a6ae612c9de516672d634c5f92c7611409
Adrien Chapelet <adrien3d@gmail.com>
2018-09-21 09:37:56 +0200

    User can see customers

--- server/router.go
+++ server/router.go
@@ -73,7 +73,7 @@ func (a *API) SetupRouter() {
 		{
 			customerController := controllers.NewCustomerController()
 			customers.Use(authMiddleware)
-			customers.Use(adminMiddleware)
+			//customers.Use(adminMiddleware)
 			customers.POST("/", customerController.CreateCustomer)
 			customers.GET("/:id", customerController.GetCustomerById)
 			customers.DELETE("/:id", customerController.DeleteCustomer)
@@ -157,6 +157,7 @@ func (a *API) SetupRouter() {
 		orders := v1.Group("/orders")
 		{
 			orders.Use(authMiddleware)
+			//customers.Use(adminMiddleware)
 			ordersController := controllers.NewOrderController()
 			orders.GET("/", ordersController.GetOrders)
 			orders.POST("/", ordersController.CreateOrder)
@@ -171,6 +172,7 @@ func (a *API) SetupRouter() {
 		warehouses := v1.Group("/warehouses")
 		{
 			warehouses.Use(authMiddleware)
+			//customers.Use(adminMiddleware)
 			warehousesController := controllers.NewWarehouseController()
 			warehouses.GET("/", warehousesController.GetWarehouses)
 			warehouses.POST("/", warehousesController.CreateWarehouse)
@@ -207,6 +209,7 @@ func (a *API) SetupRouter() {
 			geolocations.DELETE("/:id", geolocationController.DeleteGeolocation)
 
 			geolocations.Use(authMiddleware)
+			//customers.Use(adminMiddleware)
 			geolocations.GET("/user/fleets", fleetsController.GetUserFleetsGeoJSON)
 		}
 	}
