75aef1d0bdfb90925f9b6ff024c9736ce7920da5
Maxence Henneron <maxence.henneron@icloud.com>
2018-09-21 08:46:54 +0200

    The user can now see its orders, warehouses and customers

--- server/router.go
+++ server/router.go
@@ -157,7 +157,6 @@ func (a *API) SetupRouter() {
 		orders := v1.Group("/orders")
 		{
 			orders.Use(authMiddleware)
-			orders.Use(adminMiddleware)
 			ordersController := controllers.NewOrderController()
 			orders.GET("/", ordersController.GetOrders)
 			orders.POST("/", ordersController.CreateOrder)
@@ -172,7 +171,6 @@ func (a *API) SetupRouter() {
 		warehouses := v1.Group("/warehouses")
 		{
 			warehouses.Use(authMiddleware)
-			warehouses.Use(adminMiddleware)
 			warehousesController := controllers.NewWarehouseController()
 			warehouses.GET("/", warehousesController.GetWarehouses)
 			warehouses.POST("/", warehousesController.CreateWarehouse)
@@ -209,7 +207,6 @@ func (a *API) SetupRouter() {
 			geolocations.DELETE("/:id", geolocationController.DeleteGeolocation)
 
 			geolocations.Use(authMiddleware)
-			geolocations.Use(adminMiddleware)
 			geolocations.GET("/user/fleets", fleetsController.GetUserFleetsGeoJSON)
 		}
 	}
